b47bdc579ed720e2f88162f4e118bf6402e4c6e0
qwdsx <qwdsx>
2024-02-24 13:27:11 +0200

    move dummy data to a separate file

---
+++ src/app/tulokset/scores.ts
@@ -0,0 +1,108 @@
+import { Score as ScoreType} from "./types";
+
+export const scores: ScoreType[] = [
+    {
+      "year": 2024,
+      "divisions": [
+        {
+          "name": "Y-mestaruussarja",
+          "clubs": [
+            {
+              "name": "Oulun metsästys- ja ampumaseura",
+              "shortName": "OMAS",
+              "position": 1,
+              "members": [
+                {
+                  "firstName": "Etunimi",
+                  "lastName": "Sukunimi"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Y-suomisarja",
+          "clubs": [
+            {
+              "name": "Oulun metsästys- ja ampumaseura",
+              "shortName": "OMAS",
+              "position": 1,
+              "members": [
+                {
+                  "firstName": "Etunimi",
+                  "lastName": "Sukunimi"
+                },
+                {
+                  "firstName": "Etunimi",
+                  "lastName": "Sukunimi"
+                }
+              ]
+            },
+            {
+              "name": "Test club",
+              "shortName": "TEST",
+              "position": 2,
+              "members": [
+                {
+                  "firstName": "Etunimi",
+                  "lastName": "Sukunimi"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "year": 2023,
+      "divisions": [
+        {
+          "name": "Y-mestaruussarja",
+          "clubs": [
+            {
+              "name": "Oulun metsästys- ja ampumaseura",
+              "shortName": "OMAS",
+              "position": 1,
+              "members": [
+                {
+                  "firstName": "Etunimi",
+                  "lastName": "Sukunimi"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Y-suomisarja",
+          "clubs": [
+            {
+              "name": "Oulun metsästys- ja ampumaseura",
+              "shortName": "OMAS",
+              "position": 1,
+              "members": [
+                {
+                  "firstName": "Etunimi",
+                  "lastName": "Sukunimi"
+                },
+                {
+                  "firstName": "Etunimi",
+                  "lastName": "Sukunimi"
+                }
+              ]
+            },
+            {
+              "name": "Test club",
+              "shortName": "TEST",
+              "position": 2,
+              "members": [
+                {
+                  "firstName": "Etunimi",
+                  "lastName": "Sukunimi"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
