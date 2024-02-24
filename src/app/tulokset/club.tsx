6771bc4ebe5bb8f45626383fd415cf07378d3d5f
WalMaa <98175031+WalMaa@users.noreply.github.com>
2024-02-24 14:22:03 +0200

    added key prop to iterator

--- src/app/tulokset/club.tsx
+++ src/app/tulokset/club.tsx
@@ -14,8 +14,8 @@ const Club = ({ name, shortName, position, members }: ClubProps) => {
     <div className='h-full p-4 bg-slate-200 rounded-md mb-2 shadow-md ml-16'>
         <h1 className='font-medium'>{`${position}. ${name} - ${shortName}`}</h1>
             <div>
-                {members.map(member => (
-                    <Member firstName={member.firstName} lastName={member.lastName}/>
+                {members.map((member, index) => (
+                    <Member key={index} firstName={member.firstName} lastName={member.lastName}/>
                 ))}
             </div>
     </div>
