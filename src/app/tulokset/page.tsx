f5ddf722bbc9b8083b175a5746899091198518e7
qwdsx <qwdsx>
2024-03-02 21:18:51 +0200

    add big scores title h1

--- src/app/tulokset/page.tsx
+++ src/app/tulokset/page.tsx
@@ -6,6 +6,7 @@ import { scores } from './scores';
 export default function page() {
   return (
     <main className="p-2 h-screen">
+      <h1 className='text-4xl mb-2'>Tulokset</h1>
       {scores.sort((a, b,) => b.year - a.year).map((score, index) => (
         <Score key={index} year={score.year} divisions={score.divisions}/>
       ))}
