5ac6104ca6b0d388573df7552f2b91c60dc12ea1
qwdsx <qwdsx>
2024-03-02 21:20:56 +0200

    change h-screen to min-h-screen

--- src/app/tulokset/page.tsx
+++ src/app/tulokset/page.tsx
@@ -5,7 +5,7 @@ import { scores } from './scores';
 
 export default function page() {
   return (
-    <main className="p-2 h-screen">
+    <main className="p-2 min-h-screen">
       <h1 className='text-4xl mb-2'>Tulokset</h1>
       {scores.sort((a, b,) => b.year - a.year).map((score, index) => (
         <Score key={index} year={score.year} divisions={score.divisions}/>
