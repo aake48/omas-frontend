71e805a93930d053a56c7ac5671a3d9aff360fad
qwdsx <qwdsx>
2024-03-03 17:50:54 +0200

    simple fetch competition scores template?

--- src/app/tulokset/page.tsx
+++ src/app/tulokset/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client';
+import React, { useState, useEffect } from 'react'
 import { Score as ScoreType } from './types';
 import Score from './score';
 import { scores } from './scores';
 
 export default function page() {
+  const [scores1, setScores] = useState<ScoreType[]>([]);
+
+  // fetching does not do anything yet
+  useEffect(() => {
+    fetch("http://localhost:8080/api/competition/result/test_comp", {cache: 'default'})
+      .then(res => {
+        console.log(res);
+        if (!res.ok) {
+          throw new Error("could not fetch scores")
+        }
+        return res.json()
+      })
+      .then(json => {
+        setScores(json);
+      })
+      .catch(e => console.error(e))
+  }, [])
+
   return (
     <main className="p-2 min-h-screen">
       <h1 className='text-4xl mb-2'>Tulokset</h1>
