32e4dd1592e0a3cfe2b94cf95745f2555f94d0d9
qwdsx <qwdsx>
2024-03-03 19:09:16 +0200

    add types for later

---
+++ src/app/kilpailut/types.ts
@@ -0,0 +1,20 @@
+
+export interface Competition {
+    name: string,
+    nameNonId: string,
+    creationDate: string
+    teams?: Team[]
+}
+
+export interface Team {
+    club: string,
+    totalScore: number,
+    scores?: TeamScore[]
+}
+
+export interface TeamScore {
+    bullsEyeCount: number,
+    sum: number,
+    userId: number,
+    scorePerShot: string
+}
