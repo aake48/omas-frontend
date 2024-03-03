de53561e924dd3bbbc9e5ec7af67b3645dfae8a9
Jakomu <janne.komul@gmail.com>
2024-03-03 03:00:13 +0200

    Add common types.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,18 +1,23 @@
-export const contacts = [
-    { phone: '123-456-7890', email: 'toimisto@omas.fi', address: 'Osoite 123' },
-]
+import { Competition } from "@/types/commonTypes";
 
-export const upcoming = [
-    {
-        "name": "Tuleva kilpailu 1",
-        "date": "01.06.2024"
-    },
-    {
-        "name": "Tuleva kilpailu 2",
-        "date": "01.06.2024"
-    },
-    {
-        "name": "Tuleva kilpailu 3",
-        "date": "01.06.2024"
-    }
-]
+export const contacts = [
+  { phone: "123-456-7890", email: "toimisto@omas.fi", address: "Osoite 123" },
+];
+
+export const upcoming: Competition[] = [
+  {
+    id: "1",
+    name: "Tuleva kilpailu 1",
+    creationDate: new Date("01.06.2024"),
+  },
+  {
+    id: "2",
+    name: "Tuleva kilpailu 2",
+    creationDate: new Date("01.06.2024"),
+  },
+  {
+    id: "3",
+    name: "Tuleva kilpailu 3",
+    creationDate: new Date("01.06.2024"),
+  },
+];
