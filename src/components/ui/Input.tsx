cb0446c5daa23458d77f875519f932b2f7c56dea
Jakomu <janne.komul@gmail.com>
2024-03-07 22:35:36 +0200

    Add register page functionality. Install formik.

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
 
 interface InputProps {
-    id: string
-    placeholder: string
-    required: boolean
-    type: string
-
-
+  id: string;
+  placeholder: string;
+  required: boolean;
+  type: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
 }
-export default function Input({ id, placeholder, required, type}: InputProps) {
+export default function Input({
+  id,
+  placeholder,
+  required,
+  type,
+  onChange,
+  value,
+}: InputProps) {
   return (
-    <input className='py-2 px-1 min-w-0 border rounded-lg' type={type} id={id} placeholder={placeholder} required={required}/>
-  )
+    <input
+      className="py-2 px-1 min-w-0 border rounded-lg"
+      type={type}
+      id={id}
+      placeholder={placeholder}
+      required={required}
+      onChange={onChange}
+      value={value}
+    />
+  );
 }
