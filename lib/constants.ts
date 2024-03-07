6259a67bfbe1b6e4a722f65b735c1437997ff2f3
Lauri.L <late4460@gmail.com>
2024-03-07 19:41:11 +0200

    add correct types

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,23 +1,26 @@
-import { Competition } from "@/types/commonTypes";
+import { CompetitionResponse } from "@/types/commonTypes";
 
 export const contacts = [
   { phone: "123-456-7890", email: "toimisto@omas.fi", address: "Osoite 123" },
 ];
 
-export const upcoming: Competition[] = [
+export const upcoming: CompetitionResponse[] = [
   {
-    id: "1",
-    name: "Tuleva kilpailu 1",
-    creationDate: new Date("01.06.2024"),
+    name: "Tuleva_kilpailu_1",
+    nameNonId: "Tuleva kilpailu 1",
+    type:"rifle",
+    creationDate: ("01.06.2024"),
   },
   {
-    id: "2",
-    name: "Tuleva kilpailu 2",
-    creationDate: new Date("01.06.2024"),
+    name: "Tuleva_kilpailu_2",
+    nameNonId: "Tuleva kilpailu 2",
+    type:"rifle",
+    creationDate: ("01.06.2024"),
   },
   {
-    id: "3",
-    name: "Tuleva kilpailu 3",
-    creationDate: new Date("01.06.2024"),
+    name: "kesan_yo_-kilpailu",
+    nameNonId: "kesän yö -kilpailu",
+    type:"pistol",
+    creationDate: ("01.06.2024"),
   },
 ];
