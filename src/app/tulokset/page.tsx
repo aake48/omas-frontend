f8f554e3a57e4b16d3ff379f3c70470acd69c7f3
qwdsx <qwdsx>
2024-03-10 23:35:00 +0200

    past competitions in own file

--- src/app/tulokset/page.tsx
+++ src/app/tulokset/page.tsx
@@ -1,71 +1,11 @@
 'use client';
-import React, { useState, useEffect } from 'react'
-import { CompetitionResponse, QueryCompetition, competitionResults } from '@/types/commonTypes';
-import { getCompetitionsQueryUrl } from '@/types/APIConstants';
-import fetchData from './get';
-import Score from './Score';
-import Paginator from './Paginator';
+import React from 'react'
+import PastCompetitions from './PastCompetitions';
 
 export default function page() {
-  const [content, setContent] = useState<QueryCompetition>();
-  const [pageNumber, setPageNumber] = useState(0);
-  const [currentDate, setCurrentDate] = useState(new Date());
-
-  let formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
-
-  let apiUrl = getCompetitionsQueryUrl("", pageNumber, 5);
-  
-  const fetchContent = async () => {
-    const data: QueryCompetition = await fetchData(apiUrl);
-    setContent(data)
-  };
-
-  useEffect(() => {
-    fetchContent();
-  }, [pageNumber])
-  
-  if (
-    content?.content !== undefined &&
-    content.content !== null &&
-    content.content.length !== 0
-  ) {
-    let competitions = content.content;
-
-    let pastCompetitions: CompetitionResponse[] = competitions.filter(competition => {
-      return formattedDate > competition.endDate;
-    })
-    
-    let groupPastCompetitionsByYear = Map.groupBy(pastCompetitions, (comp: competitionResults) => {
-      const date = comp.creationDate.split("-")[0];
-      return date;
-    })
-
-    const handlePageNumberChange = (page: number) => {
-      if (page < 0 || page > content.totalPages - 1) return;
-      setPageNumber(page);
-    }
-
-    return (
-      <main className="min-h-screen p-4">
-        <h1 className='text-3xl mb-4'>Menneiden kilpailuiden tulokset</h1>
-        <div>
-          {Array.from(groupPastCompetitionsByYear).map((year: any) => (
-            <Score
-              year={parseInt(year[0])}
-              competitionResults={competitions}
-            />
-          ))}
-        </div>
-        <Paginator
-          pageNumber={pageNumber}
-          totalPages={content.totalPages}
-          handlePageNumberChange={handlePageNumberChange}
-        />
-      </main>
-    )
-  } else {
-    return (
-      <h1 className='text-xl'>Tuloksia ei löytynyt</h1>
-    )
-  }
+  return (
+    <main className="min-h-screen">
+      <PastCompetitions />
+    </main>
+  )
 }
