c088d55f7b02db391f66dad28aeb98511b26d657
qwdsx <qwdsx>
2024-03-19 10:28:17 +0200

    changes

--- src/app/tulokset/PastCompetitions.tsx
+++ src/app/tulokset/PastCompetitions.tsx
@@ -1,19 +1,17 @@
 'use client';
 import React, { useState, useEffect } from 'react'
 import { CompetitionResponse, QueryCompetition, competitionResults } from '@/types/commonTypes';
-import { getCompetitionsQueryUrl } from '../../lib/APIConstants';
-import fetchData from '../../lib/get';
+import fetchData from '@/lib/get';
 import Score from './Score';
 import Paginator from './Paginator';
 import { groupBy } from 'lodash';
+import { getCompetitionsQueryUrl } from '@/lib/APIConstants';
 
 const PastCompetitions = () => {
   const [content, setContent] = useState<QueryCompetition>();
   const [pageNumber, setPageNumber] = useState(0);
-  const [currentDate, setCurrentDate] = useState(new Date());
-
-  let formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
   
+  let currentDate = new Date();
   let apiUrl = getCompetitionsQueryUrl("", pageNumber, 5);
   
   const fetchContent = async () => {
@@ -32,28 +30,24 @@ const PastCompetitions = () => {
     ) {
     let competitions = content.content;
 
-    let pastCompetitions: CompetitionResponse[] = competitions.filter(competition => {
-      let date1 = new Date(competition.endDate);
-      let date2 = new Date(formattedDate);
-      return date2 > date1;
+    let pastCompetitions: CompetitionResponse[] = competitions
+      .sort((a, b) => {
+        return a.startDate.localeCompare(b.startDate);
+      })
+      .filter(competition => {
+      let compDate = new Date(competition.startDate);
+      return compDate < currentDate;
     })
 
     let groupPastCompetitionsByYear = groupBy(pastCompetitions, (comp: competitionResults) => {
-      const date = new Date(comp.endDate);
+      const date = new Date(comp.startDate);
       return date.getFullYear();
     })
 
     let groups = Object.entries(groupPastCompetitionsByYear);
 
     if (groups.length === 0) {
-      return (
-        <div className="p-4">
-        <h1 className='text-3xl mb-4'>Menneiden kilpailuiden tulokset</h1>
-        <div>
-          <h1 className='text-xl'>Tuloksia ei löytynyt</h1>
-        </div>
-      </div>
-      )
+      scoresNotFound();
     }
 
     const handlePageNumberChange = (page: number) => {
@@ -84,14 +78,10 @@ const PastCompetitions = () => {
 
 }
 
-const getCompetitionsPerYear = () => {
-  
-}
-
 const scoresNotFound = () => {
   return (
       <div className="p-4">
-      <h1 className='text-3xl mb-4'>Menneiden kilpailuiden tulokset</h1>
+      <h1 className='text-3xl mb-4'>Vimmeisimmät tulokset</h1>
       <div>
         <h1 className='text-xl'>Tuloksia ei löytynyt</h1>
       </div>
