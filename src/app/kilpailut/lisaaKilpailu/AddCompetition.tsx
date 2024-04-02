d5345ae968de1b5032b911fda684b1dbf63f7770
WalMaa <98175031+WalMaa@users.noreply.github.com>
2024-04-02 18:35:57 +0300

    reafactored to formik

--- src/app/kilpailut/lisaaKilpailu/AddCompetition.tsx
+++ src/app/kilpailut/lisaaKilpailu/AddCompetition.tsx
@@ -2,78 +2,72 @@
 
 import { Button } from "@/components/ui/Button";
 import Dropdown from "@/components/ui/Dropdown";
-import Input from "@/components/ui/Input";
 import { addCompetitionURL } from "../../../lib/APIConstants";
 import { PostCompetition } from "@/types/commonTypes";
 import axios from "axios";
-import { useState } from "react";
+import { Field, Form, Formik } from "formik";
+import CustomInput from "@/components/ui/CustomInput";
 
 export default function AddCompetition() {
-  type CompetitionType = "rifle" | "pistol";
-  const competitionTypes = ["rifle", "pistol"];
+    type CompetitionType = "rifle" | "pistol";
+    const competitionTypes = ["rifle", "pistol"];
 
-  const [competitionName, setCompetitionName] = useState("");
-  const [competitionType, setCompetitionType] =
-    useState<CompetitionType>("rifle");
+    return (
+        <div className="flex flex-col justify-center gap-2 p-5 rounded-lg shadow-lg container mx-auto">
+            <h1 className="text-2xl my-2">Kilpailun luominen</h1>
+            <Formik
+                id="addCompetitionForm"
+                initialValues={{
+                    competitionName: "",
+                    competitionType: "none",
+                }}
+                onSubmit={(values, { setSubmitting, resetForm }) => {
+                  try {
+                    const response = axios.post(addCompetitionURL, {
+                        headers: {
+                            Authorization: `Bearer ${localStorage.getItem("token")}`,
+                            "Content-Type": "application/json",
+                        },
+                        data: values as PostCompetition,
+                    });
+        
+                } catch (error: any) {
+                    if (error.response.data) {
+                        console.log(error.response.data);
+                    }
+                }
+                }}
+            >
+                <Form>
+                    <CustomInput
+                        label="Kilpailun nimi"
+                        name="competitionName"
+                        type="text"
+                        placeholder="Kilpailun nimi"
+                    />
+                    <Field name="competitiontype">
+                        {({ field, form }: any) => (
+                            <Dropdown
+                                options={competitionTypes}
+                                id={"competitionType"}
+                                selected={field.value}
+                                onChange={(e) => {
+                                    form.setFieldValue(field.name, e.target.value);
 
-  const handleSubmit = async (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    event.preventDefault();
-
-    const competition: PostCompetition = {
-      competitionName: competitionName,
-      competitionType: competitionType,
-    };
-
-    try {
-      const response = await axios.post(addCompetitionURL, competition, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (response.status === 201) {
-        console.log(response.data);
-      } else {
-        console.log(response.data.message);
-      }
-    } catch (error: any) {
-      if (error.response.data) {
-        console.log(error.response.data);
-      }
-    }
-  };
-
-  return (
-    <div className="flex flex-col justify-center gap-2 p-5 rounded-lg shadow-lg container mx-auto">
-      <form></form>
-      <h1 className="text-2xl my-2">Kilpailun luominen</h1>
-      <p className="my-2">Kilpailun nimi</p>
-      <Input
-        id="competitionName"
-        required={true}
-        placeholder={"Kilpailun nimi"}
-        type={"text"}
-        onChange={(e) => setCompetitionName(e.target.value)}
-      />
-      <Dropdown
-        options={competitionTypes}
-        id={"competitionType"}
-        onChange={(e) => {
-          setCompetitionType(e.target.value as CompetitionType);
-        }}
-        required={true}
-      ></Dropdown>
-      <Button
-        variant={"outline"}
-        className="my-4 mx-auto"
-        type="submit"
-        onClick={(e) => handleSubmit(e)}
-      >
-        Luo kilpailu
-      </Button>
-    </div>
-  );
+                                }}
+                                required={true}
+                            ></Dropdown>
+                        )}
+                    </Field>
+                    <Button
+                        variant={"outline"}
+                        className="my-4 mx-auto"
+                        type="submit"
+                    >
+                        Luo kilpailu
+                    </Button>
+                </Form>
+            </Formik>
+        </div>
+    );
 }
