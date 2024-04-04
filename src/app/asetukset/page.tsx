3c00f37db06fd3711f512069508d14792cc5a213
qwdsx <email>
2024-04-04 05:54:21 +0200

    more padding

--- src/app/asetukset/page.tsx
+++ src/app/asetukset/page.tsx
@@ -3,7 +3,7 @@ import Settings from "./Settings";
 
 export default function page() {
     return (
-        <main className="flex min-h-screen flex-col p-2 gap-8">
+        <main className="flex min-h-screen flex-col p-4">
             <Settings />
         </main>
     );
