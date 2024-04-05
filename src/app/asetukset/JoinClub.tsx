e1cc40bfec407261f965b338cb5ec5cc97f70c7f
qwdsx <email>
2024-04-05 22:03:07 +0200

    add passkey input to JoinClub

--- src/app/asetukset/JoinClub.tsx
+++ src/app/asetukset/JoinClub.tsx
@@ -11,6 +11,8 @@ const JoinClub = () => {
 
     const handleSubmit = async (data: FormData) => {
         const club = data.get("club");
+        const pass = data.get("pass") || null;
+        console.log(club, pass);
         try {
             const res = await axios({
                 method: 'post',
@@ -20,30 +22,31 @@ const JoinClub = () => {
                     'Content-Type': 'application/json'
                 },
                 data: {
-                    clubName: club
+                    clubName: club,
+                    passkey: pass
                 }
             });
 
             console.log(res);
 
             if (res.status === 200) {
-                setMessage(`Seuraan ${club} liittyminen onnistui`);
+                setMessage(`Seuraan ${club} liittyminen onnistui.`);
                 setMessageStyle("text-black");
             } else {
-                setMessage(`Seuraan ${club} liittyminen epäonnistui`);
+                setMessage(`Seuraan ${club} liittyminen epäonnistui. Tarkista, että seuran nimi sekä salasana ovat oikein ja yritä uudelleen. Salasanaa ei tarvitse syöttää, jos seura ei sitä vaadi.`);
                 setMessageStyle("text-red-500");
             }
         } catch (error) {
             console.log(error);
-            setMessage(`Seuraan ${club} liittyminen epäonnistui`);
+            setMessage(`Seuraan ${club} liittyminen epäonnistui. Tarkista, että seuran nimi sekä salasana ovat oikein ja yritä uudelleen. Salasanaa ei tarvitse syöttää, jos seura ei sitä vaadi.`);
             setMessageStyle("text-red-500");
         }
-    
+
     }
 
     return (
         <div className="mt-2">
-                <h1>Liity seuraan</h1>
+                <h1 className="text-xl">Liity seuraan:</h1>
                 <form
                     action={handleSubmit}
                     className='flex flex-col gap-2 max-w-[200px]'
@@ -53,6 +56,12 @@ const JoinClub = () => {
                         type="text"
                         name="club"
                         placeholder="seuran nimi"
+                    />
+                    <input
+                        className='border rounded-lg p-2'
+                        type="password"
+                        name="pass"
+                        placeholder="seuran salasana"
                     />
                     <Button
                         variant="outline"
