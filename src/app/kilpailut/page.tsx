4f04bb111a519046566b243e43efdb628bce57a7
Jakomu <janne.komul@gmail.com>
2024-04-14 22:37:51 +0300

    Change title to match content

--- src/app/kilpailut/page.tsx
+++ src/app/kilpailut/page.tsx
@@ -5,7 +5,6 @@ import Competitions from "./Competitions";
 import { Button } from "@/components/ui/Button";
 import Link from "next/link";
 
-
 export default function LisääKilpailu() {
   const [competitions, setCompetitions] = useState<CompetitionType[]>([]);
 
@@ -20,11 +19,12 @@ export default function LisääKilpailu() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-between p-8 sm:p-24">
       <div className="container shadow-lg p-4 sm:p-10 mx-auto">
-
         <Button variant={"outline"} className="ml-10">
           <Link href="/kilpailut/lisaaKilpailu">Lisää kilpailu</Link>
         </Button>{" "}
-        <h1 className="text-3xl text-center">Tulevat kilpailut</h1>
+        <h1 className="text-3xl text-center">
+          Aktiiviset ja tulevat kilpailut
+        </h1>
         <Competitions />
       </div>
     </main>
