eed5cf995acc0b873fc6840e334c473ebbe5c120
qwdsx <qwdsx>
2024-04-14 14:11:50 +0300

    remove teams page and change it to clubs view
    remove teams page

---
+++ src/app/seurat/JoinClub.tsx
@@ -0,0 +1,97 @@
+import { Button } from "@/components/ui/Button"
+import { joinClubURL } from "@/lib/APIConstants";
+import { User } from "@/types/commonTypes";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface JoinClubProps {
+    clubName: string
+}
+
+const JoinClub = ({ clubName }: JoinClubProps) => {
+    const [message, setMessage] = useState("");
+    const [messageStyle, setMessageStyle] = useState("text-black");
+    const [user, setUser] = useState<User>();
+
+    const token = localStorage.getItem("token");
+
+    const handleProfileUpdate = (club: string) => {
+        const newUser: User = {
+            ...JSON.parse(localStorage.getItem("userInfo")!),
+            club: club
+        };
+        localStorage.setItem("userInfo", JSON.stringify(newUser));
+    }
+
+    const handleSubmit = async (data: FormData) => {
+        // const club = data.get("club");
+        const pass = data.get("pass") || null;
+        console.log(clubName, pass);
+        try {
+            const res = await axios({
+                method: 'post',
+                url: joinClubURL,
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                },
+                data: {
+                    clubName: clubName,
+                    passkey: pass
+                }
+            });
+
+            console.log(res);
+
+            if (res.status === 200) {
+                try {
+                    handleProfileUpdate(clubName);
+                    setMessage(`Seuraan ${clubName} liittyminen onnistui.`);
+                    setMessageStyle("text-black");
+                } catch (e: any) {
+                    console.log(e);
+                }
+            } else {
+                setMessage(`Seuraan ${clubName} liittyminen epäonnistui. Tarkista, että seuran nimi sekä salasana ovat oikein ja yritä uudelleen. Salasanaa ei tarvitse syöttää, jos seura ei sitä vaadi.`);
+                setMessageStyle("text-red-500");
+            }
+        } catch (error) {
+            console.log(error);
+            setMessage(`Seuraan ${clubName} liittyminen epäonnistui. Tarkista, että seuran nimi sekä salasana ovat oikein ja yritä uudelleen. Salasanaa ei tarvitse syöttää, jos seura ei sitä vaadi.`);
+            setMessageStyle("text-red-500");
+        }
+    }
+
+    return (
+        <div>
+            <form
+                action={handleSubmit}
+                className='flex flex-col gap-2 max-w-[200px]'
+            >
+                {/* <input
+                    className='border rounded-lg p-2'
+                    type="text"
+                    name="club"
+                    placeholder="seuran nimi"
+                /> */}
+                <input
+                    className='border rounded-lg p-2'
+                    type="password"
+                    name="pass"
+                    placeholder="seuran salasana"
+                />
+                <Button
+                    variant="outline"
+                    size="sm"
+                    className="hover:bg-slate-100"
+                    type="submit"
+                >
+                    Liity seuraan
+                </Button>
+            </form>
+            <p className={`${messageStyle} mt-3`}>{message}</p>
+        </div>
+    )
+}
+
+export default JoinClub;
--- src/types/commonTypes.ts
+++ src/types/commonTypes.ts
@@ -173,6 +173,19 @@ export type QueryCompetition = {
   empty: boolean;
 };
 
+export type QueryClub = {
+  content: ClubResponse[] | null;
+  pageable: Pageable;
+  last: boolean; // isLastPage
+  totalElements: number; // how many element are there in the DB
+  totalPages: number; // how many pages are there wi
+  size: number;
+  first: true;
+  number: number;
+  numberOfElements: number; //in this page
+  empty: boolean;
+};
+
 /**
  * get competition data /api/admin/user/query?...
  */
