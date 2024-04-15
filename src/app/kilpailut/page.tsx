61a0703d29acfd1d6c1d5106a53fa4373c4405bc
Jakomu <janne.komul@gmail.com>
2024-04-15 22:27:26 +0300

    Remove unnecessary fetch

--- src/app/kilpailut/page.tsx
+++ src/app/kilpailut/page.tsx
@@ -1,21 +1,9 @@
-"use client"
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Competitions from "./Competitions";
 import { Button } from "@/components/ui/Button";
 import Link from "next/link";
 
 export default function LisääKilpailu() {
-  const [competitions, setCompetitions] = useState<CompetitionType[]>([]);
-
-  useEffect(() => {
-    fetch("https://localhost:8080/api/competition/result/kesan_ampujaiset", { cache: 'default' })
-      .then((res) => res.json())
-      .then((json) => {
-        setCompetitions([...competitions, json])
-      })
-  }, []);
-
   return (
     <main className="flex flex-col min-h-screen items-center justify-between p-8 sm:p-24">
       <div className="container shadow-lg p-4 sm:p-10 mx-auto">
