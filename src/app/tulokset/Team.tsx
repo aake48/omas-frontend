701ea110a436b1a5670bbd5586a54561367f304c
WalMaa <98175031+WalMaa@users.noreply.github.com>
2024-04-26 14:17:03 +0300

    total points to tulokset

--- src/app/tulokset/Team.tsx
+++ src/app/tulokset/Team.tsx
@@ -1,39 +1,52 @@
-import React from 'react'
-import Member from './Member'
-import { competitionResultsUser } from '@/types/commonTypes'
+import React from "react";
+import Member from "./Member";
+import { competitionResultsUser } from "@/types/commonTypes";
 
 interface TeamProps {
-  teamDisplayName: string,
-  position: number,
-  scores: competitionResultsUser[] | null
+    teamDisplayName: string;
+    position: number;
+    scores: competitionResultsUser[] | null;
 }
 
 const Team = ({ teamDisplayName, position, scores }: TeamProps) => {
-  if (scores !== null) {
-    return (
-      <div data-testid={`team-${position}`}>
-        <h1 className='font-medium'>{`${position + 1}. ${teamDisplayName}`}</h1>
-        <div>
-          {scores.map((member, index) => (
-            <Member
-              key={index}
-              name={member.name}
-              score={member.sum}
-            />
-          ))}
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1 className='font-medium'>{`${position + 1}. ${teamDisplayName}`}</h1>
-        <div>
-          <p>Virhe joukkueen jäsenten haussa</p>
-        </div>
-      </div>
-    )
-  }
-}
+    if (scores !== null) {
+      console.log(scores);
+        const totalScores = scores.reduce(
+            (acc, member) => acc + member.sum,
+            0
+        );
+
+        return (
+            <div data-testid={`team-${position}`}>
+                <div className="flex gap-5">
+                    <h1 className="font-medium">{`${
+                        position + 1
+                    }. ${teamDisplayName}`}</h1>
+                    <p className="font-bold">{totalScores}p</p>
+                </div>
+                <div>
+                    {scores.map((member, index) => (
+                        <Member
+                            key={index}
+                            name={member.name}
+                            score={member.sum}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1 className="font-medium">{`${
+                    position + 1
+                }. ${teamDisplayName}`}</h1>
+                <div>
+                    <p>Virhe joukkueen jäsenten haussa</p>
+                </div>
+            </div>
+        );
+    }
+};
 
 export default Team;
