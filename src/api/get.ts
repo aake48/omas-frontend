78222887e0a55c0ad6a0f4ca5126a3f587f6faa3
Aleksi Santala <asantala22@student.oulu.fi>
2025-02-27 15:03:33 +0200

    get.ts fetch error fix

--- src/api/get.ts
+++ src/api/get.ts
@@ -1,16 +1,24 @@
+import axios from 'axios'
+import https from 'https'
 export default async function fetchData(url: string): Promise<any> {
     try {
-        const response = await fetch(url, {
+        console.log(url)
+        const response = await axios(url, {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
             },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false
+            }),
         });
 
-        if (!response.ok) {
-            throw new Error('HTTP error! status: ' + response.status + ' ' + response.statusText);
-        }
+        //if (!response.ok) {
+        //    throw new Error('HTTP error! status: ' + response.status + ' ' + response.statusText);
+        //}
 
-        return await response.json();
+        //return await response.json();
+        return await response.data
     } catch (error) {
         console.error('Error:', error);
         throw Error('Virhe haettaessa tietoja palvelimelta');
