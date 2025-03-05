186f80224fe81509e05d12d74c40a3640845a5f9
Aleksi Santala <asantala22@student.oulu.fi>
2025-03-05 20:06:13 +0200

    filter competitions by series feature

---
+++ src/components/ui/SeriesDropdown.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface DropdownProps {
+    id?: string;
+    disabled?: boolean;
+    options: string[];
+    required?: boolean;
+    selected?: any;
+    onChange?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+    value?: any;
+}
+
+export default function SeriesDropdown({
+    id,
+    disabled = false,
+    options,
+    required = false,
+    selected,
+    onChange,
+}: DropdownProps) {
+    return (
+        <select
+            id={id ? id : "dropdown"}
+            disabled={disabled}
+            required={required}
+            className="py-2 text-xl bg-white placeholder:bg-gray-500 border-slate-500 text-black px-3 min-w-0 border rounded-lg"
+            value={selected ?? "none"}
+            onChange={onChange}
+        >
+            <option key={"default"} value="none" hidden disabled>
+                Valitse
+            </option>
+            {options.map((option) => (
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
+    );
+}
