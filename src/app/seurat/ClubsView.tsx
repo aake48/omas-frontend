51c5cdba151c90bf7be98b6f8f18d978ce65e45e
aake48 <131197767+aake48@users.noreply.github.com>
2025-03-24 12:52:07 +0200

    merge conflict resolved

--- src/app/seurat/ClubsView.tsx
+++ src/app/seurat/ClubsView.tsx
@@ -47,7 +47,7 @@ const ClubsView = () => {
                 if (storedUser?.club) setJoinedClub(storedUser.club);
 
                 if (storedUser?.roles) {
-                    const roles:String[] = storedUser.roles.replace(/[[\]]/g, "").split(",");
+                    const roles:string[] = storedUser.roles.replace(/[[\]]/g, "").split(",");
                     const adminRoles = roles
                         .filter(role => role.endsWith("/admin"))
                         .map(role => role.replace("/admin", "").trim());
@@ -76,7 +76,7 @@ const ClubsView = () => {
 	return (
         <main className="flex min-h-screen flex-col items-center p-4 gap-10">
             <h1 className="text-4xl">Seurat</h1>
-                <Input id="search" placeholder="Hae seuraa" required={false} type="text" onChange={(e) => setSearch(e.target.value)} />
+                <Input id="search" placeholder="Hae seuraa" type="text" onChange={(e) => setSearch(e.target.value)} required={false} />
                 <Paginator pageNumber={pageNumber} totalPages={data.totalPages} handlePageNumberChange={handlePageNumberChange} />
                 <div className="flex flex-col gap-2 w-full">
                     {data.content?.map((club: ClubResponse) => (
