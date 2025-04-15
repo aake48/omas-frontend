b47bfa29277df22a4ccb3ec98a56e92215699e29
aake48 <131197767+aake48@users.noreply.github.com>
2025-04-15 11:51:32 +0300

    made score input more clear

--- src/app/ilmoitus/page.tsx
+++ src/app/ilmoitus/page.tsx
@@ -8,6 +8,7 @@ export default async function Ilmoitus() {
         <main className="flex md:min-h-screen flex-col p-5 items-center md:p-24">
             <div className="flex flex-col  items-center gap-5 md:gap-10  justify-center p-5 md:p-10 rounded-lg shadow-lg container mx-auto">
                 <h1 className="text-center text-3xl">Tuloksen ilmoittaminen</h1>
+                <h2 className="text-center text-2xl">Valitse tuloksen tyyppi:</h2>
                 <ClientWrapper />
             </div>
         </main>
