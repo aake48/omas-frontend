cdd5df418c24a7dbef588a6deface2d620f84344
nsiltala <144348410+nsiltala@users.noreply.github.com>
2025-04-18 14:12:55 +0300

    Centered club elements; Implemented notifications

--- src/app/seurat/Club.tsx
+++ src/app/seurat/Club.tsx
@@ -1,7 +1,6 @@
 import JoinClub from "./JoinClub";
 import ChangeClubKey from "./ChangeClubKey";
-import { useEffect, useState } from "react";
-import { ClubResponse, User } from "@/types/commonTypes";
+import { ClubResponse} from "@/types/commonTypes";
 import { formatDate } from "@/lib/utils";
 import useUserInfo from "@/lib/hooks/get-user.info";
 
@@ -16,17 +15,20 @@ const Club = ({ club, clubAdminRoles, joinedClub, setJoinedClub }: ClubProps) =>
     const { token } = useUserInfo();
 
     return (
-        <div className="shadow p-2">
+        <div className="shadow p-4 rounded text-center">
             <div>
-                <p className="text-xl">{club.nameNonId}</p>
+                <p className="text-xl font-semibold">{club.nameNonId}</p>
                 <p className="text-slate-700">Seura luotu: {formatDate(club.creationDate)}</p>
             </div>
-            <div>
-                {/* Pass `joinedClub` state to JoinClub */}
-                <JoinClub id={club.name} joinedClub={joinedClub} setJoinedClub={setJoinedClub} />
-
-                {/* Display ChangeClubKey only for club admins */}
-                {clubAdminRoles.includes(club.name) && <ChangeClubKey token={token} clubName={club.name} />}
+            <div className="mt-4 flex flex-col items-center gap-2">
+                <JoinClub
+                    id={club.name}
+                    joinedClub={joinedClub}
+                    setJoinedClub={setJoinedClub}
+                />
+                {clubAdminRoles.includes(club.name) && (
+                    <ChangeClubKey token={token} clubName={club.name} />
+                )}
             </div>
         </div>
     );
